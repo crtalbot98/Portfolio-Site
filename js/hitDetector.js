a6fa90136111045f5180bb44d11300779d6636bf
Chandler Talbot <crtalbot98@gmail.com>
2021-01-13 20:43:53 -0500

    hit detection and clustering works better

--- js/hitDetector.js
+++ js/hitDetector.js
@@ -1,28 +1,65 @@
 class hitDetector{
 
-    detectHit(newCircle, list){
-        let touching = [];
-        let i = 0;
+    constructor() {
+        this.clusters = []
+    }
 
-        for(i; i < list.length; i++){
-            const circle = list[i];
-            const dx = newCircle.x - circle.x;
-            const dy = newCircle.y - circle.y;
-            const dist = Math.sqrt(dx * dx + dy * dy);
+    createClusters(list, amt){
 
-            if(dist < newCircle.r + circle.r){
-                touching.push(circle);
-                touching.push(newCircle);
+        for(let i = amt-1; i >= 0; i--){
+            let c1 = list[i];
+            let cluster = [];
+
+            for(let j = i-1; j >= 0; j--){
+                if(this.detectHit(c1, list[j])){
+                    cluster.push(list[j]);
+                }
+            }
+
+            if(cluster.length > 1){
+                let clusterExists = false;
+                if(this.clusters.length > 1){
+                    for(let k = 0; k < this.clusters.length; k++){
+                        if(this.clusters[k].includes(c1)){
+                            this.clusters[k].concat(cluster);
+                            clusterExists = true
+                        }
+                    }
+                }
+                if(!clusterExists){
+                    cluster.push(c1);
+                    this.clusters.push(cluster);
+                }
+                list.pop();
             }
         }
 
-        if(touching.length > 1){
-            list.splice(i,1);
-            list.push(touching)
+        this.correctColors();
+        return this.clusters
+    }
+
+    detectHit(c1, c2){
+        const dx = c1.x - c2.x;
+        const dy = c1.y - c2.y;
+        const dist = Math.sqrt(dx * dx + dy * dy);
+
+        if(dist < c1.r + c2.r) return true;
+        return false
+    }
+
+    correctColors(){
+        for(let i = 0; i < this.clusters.length; i++){
+            const r = this.genRandomFromInterval(220, 255);
+            const g = this.genRandomFromInterval(32, 78);
+            const b = this.genRandomFromInterval(33, 79);
+            for(let k = this.clusters[i].length-1; k >= 0; k--){
+                this.clusters[i][k].setColor(r,g,b)
+            }
         }
-        else{
-            list.push(newCircle)
-        }
+    }
+
+    genRandomFromInterval(min, max){
+        return Math.ceil(Math.random() * (max - min + 1) + min)
     }
 }
 
